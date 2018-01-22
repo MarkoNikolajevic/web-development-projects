6f0c4b115634e6904fc61357ba66703edd237465
Mark3z <mrknikolajevic@gmail.com>
2018-01-22 15:26:05 +0100

    Improve Random quote project

--- randomQuote/gulpfile.js
+++ randomQuote/gulpfile.js
@@ -5,5 +5,52 @@ const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const uglify = require("gulp-uglify");
 const htmlmin = require("gulp-htmlmin");
+const pump = require("pump");
 const browserSync = require("browser-sync").create();
 const rename = require("gulp-rename");
+
+
+// Compile scss files into css and minify it
+gulp.task("sass", function() {
+  return gulp.src("src/assets/style/sass/**/*.sass")
+    .pipe(sass({
+      outputStyle: "compressed"
+    }))
+    .on("error", sass.logError)
+    .pipe(autoprefixer({
+      browsers: "last 15 versions"
+    }))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest("dist/assets/style/css"))
+    .pipe(browserSync.stream());
+});
+
+// Minify js files
+gulp.task("minifyJs", function(cb) {
+  pump([
+    gulp.src("src/assets/js/*.js"),
+    uglify(),
+    rename({
+      suffix: ".min"
+    }),
+    gulp.dest("dist/assets/js"),
+    browserSync.stream()
+  ], cb);
+});
+
+// Browser Sync
+gulp.task("serve", ["sass", "minifyJs"], function() {
+  browserSync.init({
+    server: {
+      index: "index.html"
+    }
+  });
+  gulp.watch("src/assets/style/sass/**/*.sass", ["sass"]);
+  gulp.watch("src/assets/js/*.js").on("change", browserSync.reload);
+  gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+// Default
+gulp.task("default", ["serve"]);
